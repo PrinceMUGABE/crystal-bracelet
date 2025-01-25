63dcaf23039933d5ce6d82ace700215d407af7ca
PrinceMUGABE <princemugabe568@gmail.com>
2025-01-25 10:09:08 +0200

    added a custom map marker icon

--- src/components/Footer/MapComponent.jsx
+++ src/components/Footer/MapComponent.jsx
@@ -10,6 +10,17 @@ const MapComponent = () => {
       return; // If the map is already initialized, return early
     }
 
+    // Fix for missing marker icon images in production (Netlify)
+    const defaultIcon = new L.Icon({
+      iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images marker-icon.png", // URL for the default marker icon
+      iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png", // URL for the retina marker icon
+      shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png", // URL for the marker shadow
+      iconSize: [25, 41], // Size of the marker
+      iconAnchor: [12, 41], // Anchor position of the marker
+      popupAnchor: [1, -34], // Position of the popup
+      shadowSize: [41, 41], // Size of the shadow
+    });
+
     // Initialize the map
     const map = L.map("map", {
       center: [-1.908585181820936, 30.06428949699932], // New coordinates for the center of the map
@@ -22,8 +33,8 @@ const MapComponent = () => {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    // Add a marker at the new location
-    L.marker([-1.908585181820936, 30.06428949699932])
+    // Add a marker at the new location with the custom icon
+    L.marker([-1.908585181820936, 30.06428949699932], { icon: defaultIcon })
       .addTo(map)
       .bindPopup("<b>Our Location</b><br>.")
       .openPopup();
